4eb2f49ba770c6526baeb0985170c4f56090dc15
Buffum <colin.buffum@gmail.com>
2026-01-20 16:00:21 -0500

    refactor: simplify sprite function in SideElement component for improved performance

--- src/components/pet/Screen/SideElement.tsx
+++ src/components/pet/Screen/SideElement.tsx
@@ -1,16 +1,12 @@
-import { useMemo } from "react";
-
 export const SideElement = (props: { sprite?: string, type: 'prop' | 'status' }) => {
-
-
-    const sprite = useMemo(() => {
+    const sprite = () => {
         let root = '/assets';
         if (!props.sprite) return `${root}/blank.png`;
         root += props.type === 'prop' ? '/props' : '/statuses';
         return `${root}/${props.sprite}.png`;
-    }, [props.sprite]);
+    };
 
     return (
-        <img className={`${props.sprite || ''} ${props.sprite ? props.type : 'spacer'}`.trim()} src={sprite} />
+        <img className={`${props.sprite || ''} ${props.sprite ? props.type : 'spacer'}`.trim()} src={sprite()} />
     );
 };
