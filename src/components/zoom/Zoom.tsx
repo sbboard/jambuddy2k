00a9855e62f5bdfbe22a3bf6b42741dbf59988e4
Buffum <colin.buffum@gmail.com>
2026-01-20 23:25:32 -0500

    fix: streamline animation end event handling in Zoom component

--- src/components/zoom/Zoom.tsx
+++ src/components/zoom/Zoom.tsx
@@ -3,15 +3,13 @@ import './Zoom.scss';
 import useScale from '../../hooks/useScale';
 
 export const Zoom = () => {
-    const zoomRef = useRef<HTMLDivElement | null>(null);
 
     const { scaleValue } = useScale();
     let lastScale = useRef(scaleValue);
     let [zoom, setZoom] = useState('');
 
-    zoomRef.current?.addEventListener('animationend', () => {
-        if (zoomRef.current) setZoom('');
-    });
+    const zoomRef = useRef<HTMLDivElement | null>(null);
+    zoomRef.current?.addEventListener('animationend', () => zoomRef.current && setZoom(''));
 
     useEffect(() => {
         if (scaleValue > lastScale.current) setZoom('zoom-in');
